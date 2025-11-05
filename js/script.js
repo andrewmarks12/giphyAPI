5cbd112d96c157d8420e09f664b36d2dc91667d3
Andrew Marks <andrew_marks@student.uml.edu>
2025-11-05 16:36:10 -0500

    added my url as a constant value in script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,3 @@
 console.log("script.js loaded");
+
+const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=DqWi1st8YrW6yLyn3A8jYaSStWJieyh4&q=hockey&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
