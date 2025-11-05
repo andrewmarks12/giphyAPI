8197483748f5d5fd5a12ad1c2288cf625bacf34d
Andrew Marks <andrew_marks@student.uml.edu>
2025-11-05 17:20:39 -0500

    Gif is refreshed when the page is refreshed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-console.log("script.js loaded");
 
 const endpoint = "https://api.giphy.com/v1/gifs/search?api_key=DqWi1st8YrW6yLyn3A8jYaSStWJieyh4&q=hockey&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips";
+
+/*let data = fetch(endpoint);
+console.log(data);*/
+
+async function getGif() {
+    const response = await fetch(endpoint);
+    const data = await response.json();
+    console.log(data);
+
+    const gifs = data.data;
+    const randomGif = gifs[Math.floor(Math.random() * gifs.length)];
+
+    const gifUrl = randomGif.images.original.url;
+
+    const img = document.getElementById("GIF");
+    img.src = gifUrl;
+}
+
+
+
+getGif();
